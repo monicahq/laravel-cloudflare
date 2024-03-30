b3d7158733fd73c1aa3702e5b5aa6f295be8178a
Alexis Saettler <alexis@saettler.org>
2024-03-30 10:57:00 +0100

    fix

--- tests/Unit/Http/Middleware/TrustProxiesTest.php
+++ tests/Unit/Http/Middleware/TrustProxiesTest.php
@@ -4,7 +4,6 @@ namespace Monicahq\Cloudflare\Tests\Unit\Http\Middleware;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Cache;
-use Illuminate\Support\Facades\Route;
 use Monicahq\Cloudflare\Http\Middleware\TrustProxies;
 use Monicahq\Cloudflare\LaravelCloudflare;
 use Monicahq\Cloudflare\Tests\FeatureTestCase;
@@ -83,6 +82,8 @@ class TrustProxiesTest extends FeatureTestCase
     /** @test */
     public function it_sets_remote_addr()
     {
+        config(['laravelcloudflare.replace_ip' => true]);
+
         $request = new Request();
         $request->server->set('REMOTE_ADDR', '127.0.0.1');
         $request->headers->set('Cf-Connecting-Ip', '127.0.1.1');
